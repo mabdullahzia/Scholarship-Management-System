e88bdc08714c0a1c4bb22ab09087ae24e8c2a7fb
Muhammad Abdullah Zia <abdullah2006zia@gmail.com>
2026-02-15 13:00:10 +0500

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,26 @@
 #include<iostream>
-#include<vector>
-#include"Student.h"
+#include"Scholarship.h"
 using namespace std;
 #include"Scholarship.cpp"
 
 int main(){
-    vector<Student> students; int choice;
+    ScholarshipSystem sys;
+    int choice;
     do{
-        cout<<"1.AddStudent 2.ViewAll 0.Exit:";cin>>choice;
+        cout<<"1.Add Student 2.View All 3.View By Dept 4.Top Scholarships 0.Exit:";cin>>choice;
         switch(choice){
             case 1:{
-                string n; float m; cin.ignore();
-                cout<<"Name:";getline(cin,n);
+                string n; int d; float m; char need;
+                cout<<"Name:";cin.ignore();getline(cin,n);
+                cout<<"Dept(0-CS 1-Eng 2-Med):";cin>>d;
                 cout<<"Marks:";cin>>m;
-                Student s(n,m); s.calculateScholarship();
-                students.push_back(s); break;
+                cout<<"Need-based? (y/n):";cin>>need;
+                sys.addStudent(n,d,m,(need=='y'||need=='Y'));
+                break;
             }
-            case 2: for(auto &s:students) s.display(); break;
+            case 2: sys.displayAll(); break;
+            case 3: int d; cout<<"Dept:";cin>>d; sys.displayByDepartment(d); break;
+            case 4: int n; cout<<"Top N students:";cin>>n; sys.displayTopScholarships(n); break;
         }
     }while(choice!=0);
 }
